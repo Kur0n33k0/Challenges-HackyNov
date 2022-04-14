2f0df6086102eb1847a29fa3e9de66367f623991
Kur0n33k0 <antoinesirvenpro@gmail.com>
2022-04-14 13:23:23 +0200

    Revert "Adding HackyBot code"
    This reverts commit 09a19b0c98a09f79c397aaf51c257cc3955b2196.

--- 1rst Edition - 2022/HackyBot/private-message.js
+++
@@ -1,9 +0,0 @@
-module.exports = (client, triggerText, replyText) => {
-    client.on('messageCreate', (message) => {
-        if (message.content.toLowerCase() === triggerText.toLowerCase()){
-            message.author.send(replyText)
-            .catch(() => message.channel.send("Impossible de te parler ! Ouvre tes DM ;)"));
-            message.delete()
-        }
-    })
-}
--- 1rst Edition - 2022/HackyBot/start.sh
+++
@@ -1,9 +0,0 @@
-# /bin/sh
-while true
-do
-echo Starting Hacky Bot
-ts-node index.ts
-echo Restarting Hacky Bot in 5 Seconds...
-sleep 5
-done
-
